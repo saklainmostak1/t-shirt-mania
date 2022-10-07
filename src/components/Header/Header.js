b6c8c34c45f012c88a22d830e873d54cc93b24e9
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-08 02:55:40 +0600

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <nav>
+        
+            <nav className='header'>
                 <Link to='/'>Home</Link>
                 <Link to='/orders'>Orders</Link>
+                <Link to='/grandpa'>Grandpa</Link>
                 <Link to='/about'>About</Link>
                 <Link></Link>
             </nav>
-        </div>
+       
     );
 };
 
---
+++ src/components/MySelf/MySelf.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MySelf = ({house}) => {
+    return (
+        <div>
+            <h4>My Self</h4>
+            <p><small>House: {house} </small></p>
+        </div>
+    );
+};
+
+export default MySelf;
